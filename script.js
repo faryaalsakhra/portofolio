fed1c3c5f23eb8c18cc437988e0112a064f06f33
Faryaal Sakhra <seraphinalune6@gmail.com>
2025-08-20 02:03:01 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Theme toggle script
 const toggleBtn = document.getElementById("theme-toggle");
 const body = document.body;
 
@@ -6,7 +5,6 @@ toggleBtn.addEventListener("click", () => {
   body.classList.toggle("dark");
   body.classList.toggle("light");
 
-  // Change button icon
   if (body.classList.contains("dark")) {
     toggleBtn.textContent = "☀️";
   } else {
